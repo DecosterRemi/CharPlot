74bdd7968e98aa7f412c8a94b02d09390ff180ed
Romain Pic <romainpic@outlook.com>
2020-12-03 18:28:15 +0100

    Documentation de multivarie
    documentation roxygen2 et summary

--- R/multivarie.R
+++ R/multivarie.R
@@ -1,14 +1,18 @@
-library(rlist)
-library(FactoMineR)
-library(readxl)
-library(factoextra)
-library(ggplot2)
-library(gridExtra)
-library(ggforce)
-library(ggrepel)
-library(caret)
-library(e1071)
 
+#' `circleFun` returns the x and y coordinates of a circle.
+#'
+#' @param center Vector with the coordinates of the center. Default value is (0,0)
+#' @param diameter Diameter of the circle. Default value is 2.
+#' @param npoints Number of points used to create the circle. Default value is 100.
+#'
+#' @return data.frame with the coordinates of the points of the circle.
+#' 
+#' @import stats
+#'
+#' @examples 
+#' circle <- circleFun()
+#' plot(circle)
+#' 
 circleFun <- function(center = c(0,0),diameter = 2, npoints = 100){
   r = diameter / 2
   tt <- seq(0,2*pi,length.out = npoints)
@@ -17,10 +21,37 @@ circleFun <- function(center = c(0,0),diameter = 2, npoints = 100){
   return(data.frame(x = xx, y = yy))
 }
 
+
+#' Creation of the list of correlations and the list of squared correlations.
+#' 
+#' This function is used in `multivariate_charac()`.
+#'
+#' @param X data.frame with the predictive variable that were used for the clustering
+#' @param data List of data.frame for each clusters.
+#' @param clusters_name Vector of the names of the clusters
+#'
+#' @return List with $list_corr (list of correlations) and $list_corr_carr (list of squared correlations).
+#' @import stats 
+#'
+#' @examples
+#' require(readxl)
+#' df<- read_excel('../Autos.xlsx') #specify the path
+#' X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
+#' clus<- kmeans(X,3)
+#' y <-clus$cluster
+#'
+#'
+#' data<-list()
+#' clusters_name<-sort(unique(y))
+#' for (e in clusters_name){
+#'   data=list.append(data,X[y==e,])
+#' }
+#' 
+#' correlation_circle(X,data,clusters_name)
+#' 
 correlation_circle <- function(X,data,clusters_name){
   
   # Creation of empty lists
-  liste_graph=vector(mode='list',length = length(data)+1)
   liste_corrc=vector(mode='list',length = length(data)+1)
   liste_corr_carr=vector(mode='list',length = length(data)+1)
   
@@ -31,7 +62,7 @@ correlation_circle <- function(X,data,clusters_name){
   corr=as.data.frame(eval_norm*res.acp$loadings[])
   
   # Squared Correlation
-  corr=corr[,1:2]/sqrt(corr[,1]^2+corr[,2]^2)
+  corr=corr[,1:2]/sqrt(corr[,1]^2+corr[,2]^2) #normalization of the eigenvectors
   corr_carr=corr^2
   corr_carr$CTR1=corr_carr$Comp.1/(eval_norm[1])^2
   corr_carr$CTR2=corr_carr$Comp.2/(eval_norm[2])^2
@@ -66,26 +97,60 @@ correlation_circle <- function(X,data,clusters_name){
   return(list(list_corr=liste_corrc,list_corr_carr=liste_corr_carr))
 }
 
-multivariate_charac <- function(X,y,y_true=NULL,metric='euclidian'){
 
-  #splitting the data according to clustering
-  data<-list()
+
+#' Multivariate characterization of the clustering
+#'
+#' @param X data.frame with the predictive variable that were used for the clustering
+#' @param y data.frame (or vector) with the same number of rows (or length) as `X`. This represents the predicted classes.
+#' @param y_true data.frame (or vector) with the same number of rows (or length) as `X`. This represents the predicted classes.
+#' @param metric Metric used for the distance matrix. Can be chosen among : "euclidean", "maximum", "manhattan", "canberra", "binary" and "minkowski". Default value is "euclidean".
+#'
+#' @return multivariate - object containing descriptors of the multivariate characterization.
+#' @details multivariate objects have the following attributes :
+#' \itemize{
+#'  \item{"x"}{data.frame of the predictive variables.}
+#'  \item{"y"}{data.frame or vector of the clustering.}
+#'  \item{"y_true"}{(Optionnal) data.frame or vector of the true clustering.}
+#'  \item{"clusters_name"}{Vector of the names of the clusters.}
+#'  \item{"distance_matrix"}{Matrix of the distances between the center of clusters.}
+#'  \item{"correlation"}{List of the correlations of the whole data and in each cluster.}
+#'  \item{"squared_correlation"}{List of the squared correlations of the whole data and in each cluster.}
+#'  \item{"confusion_matrix"}{If y_true was provided, this is the confusionMatrix between the predicted clustering and the real one.}
+#' }
+#' 
+#' @import stat caret e1071 rlist
+#'
+#' @examples
+#' require(readxl)
+#' df<- read_excel('../Autos.xlsx') #specify the path
+#' X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
+#' ## Creating a clustering using k-means with k=3
+#' clus<- kmeans(X,3)
+#' y <-clus$cluster
+#' 
+#' multivariate=multivariate_charac(X,y)
+#' summary(multivariate)
+#' 
+multivariate_charac <- function(X,y,y_true=NULL,metric='euclidean'){
+  
   clusters_name<-sort(unique(y))
-  for (e in clusters_name){
-    data=list.append(data,X[y==e,])
+  #splitting the data according to clustering
+  data<-vector(mode='list',length = length(clusters_name))
+  
+  for (i in 1:length(clusters_name)){
+    data[[i]]=X[y==clusters_name[i],]
   }
 
   #centers of the clusters
-  mean_data=c()
-  for (e in data){
-    mean_data=list.append(mean_data,as.matrix(sapply(e,mean)))
+  mean_data<-c()
+  for (i in 1:length(data)){
+    mean_data=list.append(mean_data,as.matrix(sapply(data[[i]],mean)))
   }
   mean_data=matrix(mean_data,nrow = length(clusters_name))
 
   #distance matrix
   dm<-dist(mean_data,diag=T,method=metric)
-  #rownames(dm) <- clusters_name
-  #colnames(dm) <- clusters_name
 
   temp<-correlation_circle(X,data,clusters_name)
   list_corr=temp$list_corr
@@ -104,6 +169,27 @@ multivariate_charac <- function(X,y,y_true=NULL,metric='euclidian'){
 
 }
 
+
+#' Print for confusion.matrix object
+#'
+#' @param cm confusion.matrix object
+#'
+#' @return None
+#' Prints an Error message if the true clustering was not given in `multivariate_charac()`.
+#' Otherwise, it prints the confusion matrix using the same method as the class confusionMatrix.
+#' 
+#' @export
+#'
+#' @examples
+#' require(readxl)
+#' df<- read_excel('../Autos.xlsx') #specify the path
+#' X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
+#' clus<- kmeans(X,3)
+#' y <-clus$cluster
+#' 
+#' multivariate=multivariate_charac(X,y)
+#' print(multivariate$confusion_matrix)
+#' 
 print.confusion.matrix <- function(cm){
   if (length(cm)==1){
     cat('This confusion matrix is NULL. This may be due to the fact that you did not give y_true.')
@@ -115,7 +201,30 @@ print.confusion.matrix <- function(cm){
 }
 
 
+#' Plotting graphs to compare the correlation
+#' 
+#' Plotting the correlations in the whole dataset and within each cluster in order to find characteristic specific to clusters.
+#'
+#' @param list_corr list.corr object - List of the correlations of the whole data and in each cluster.
+#'
+#' @return None
+#' 
+#' @import ggplot2 ggrepel ggforce gridExtra
+#'
+#' @examples
+#' require(readxl)
+#' df<- read_excel('../Autos.xlsx') #specify the path
+#' X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
+#' ## Creating a clustering using k-means with k=3
+#' clus<- kmeans(X,3)
+#' y <-clus$cluster
+#' 
+#' multivariate=multivariate_charac(X,y)
+#' plot(multivariate$correlation)
+#' 
 plot.list.corr <- function(list_corr){
+  
+  liste_graph=vector(mode='list',length = length(data)+1)
   
   #Adding the center of the arrows
   for (i in 1:length(list_corr)) {
@@ -137,15 +246,53 @@ plot.list.corr <- function(list_corr){
 }
 
 
-setwd('C:/Users/romai/Documents/PHYTEM/2020-2021/Cours/Programmation en R/Projet/')
-df<- read_excel('Autos.xlsx')
-X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
-clus<- kmeans(X,3)
-y <-clus$cluster
+#' Summary of the object multivariate
+#' 
+#' Prints various attributes and plot the correlation of multivariate.
+#'
+#' @param multivariate object obtained as the output of `multivariate_charac`
+#'
+#' @return None
+#'
+#' @examples
+#' require(readxl)
+#' df<- read_excel('../Autos.xlsx') #specify the path
+#' X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
+#' ## Creating a clustering using k-means with k=3
+#' clus<- kmeans(X,3)
+#' y <-clus$cluster
+#' 
+#' multivariate=multivariate_charac(X,y)
+#' summarize(multivariate)
+#' 
+summary.multivariate <- function(multivariate){
+  cat("Distance Matrix :",'\n')
+  print(multivariate$distance_matrix)
+  cat("\n")
+  
+  cat("Confusion Matrix :",'\n')
+  print(multivariate$confusion_matrix)
+  cat("\n")
+  
+  cat("Correlations :",'\n')
+  print(multivariate$correlation)
+  plot(multivariate$correlation)
+  cat("\n")
+  
+  cat("Squared Correlations :",'\n')
+  print(multivariate$squared_correlation)
+}
 
-multivariate=multivariate_charac(X,y)
-print(multivariate$distance_matrix)
-print(multivariate$confusion_matrix)
-print(multivariate$correlation)
-print(multivariate$squared_correlation)
-plot(multivariate$correlation)
+# setwd('C:/Users/romai/Documents/PHYTEM/2020-2021/Cours/Programmation en R/Projet/')
+# df<- read_excel('Autos.xlsx')
+# X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower','city-mpg','highway-mpg')]
+# clus<- kmeans(X,3)
+# y <-clus$cluster
+# 
+# multivariate=multivariate_charac(X,y)
+# 
+# print(multivariate$distance_matrix)
+# print(multivariate$confusion_matrix)
+# print(multivariate$correlation)
+# print(multivariate$squared_correlation)
+# plot(multivariate$correlation)
