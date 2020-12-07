c08987677869f5125769f81aa6db429bfd8ac945
DecosterRemi <75371800+DecosterRemi@users.noreply.github.com>
2020-12-07 23:47:57 +0100

    Add tests
    And correlation barplot

--- tutorial.R
+++ tutorial.R
@@ -1,5 +1,4 @@
 library(ClusterCharPlot)
-library(readxl)
 
 
 library(readxl)
@@ -11,6 +10,11 @@ X<-df[,c('length','width','height','engine-size','compression-ratio','horsepower
 clus<- kmeans(X,3)
 y <-clus$cluster
 
+sep = sep_data(df)
+num = df[sep[[2]]]
+num
+clus<- kmeans(num,3)
+y <-clus$cluster
 ####Pre-processing####
 #transform data
 ######################
@@ -20,15 +24,26 @@ y <-clus$cluster
 ##Analyse univarié
 #Création de l'objet de la classe UniChar
 obj <- Perform_UniChar(df,y)
+plot(obj)
 
 #Qualitative
 vCramer <- vcramer.UniChar(obj,y)
 print(vCramer)
+vCramer["label"] <- obj$catnames
+colnames(vCramer[1])
+barplt.UniChar(obj, vCramer, TRUE)
+
 
 #Quantitative
 #Correlation, caracterisation des partitions
 cor <- correlation(obj,y)
 print(cor)
+
+tmp <- as.data.frame(cor)
+(correlations <- t(tmp[4,]))
+print(t(correlations))
+ClusterCharPlot::barplt.UniChar(obj, correlations)
+obj$colnames
 
 #Valeur test, caracterisation des groupes
 vt <- valuetest(obj,y)
